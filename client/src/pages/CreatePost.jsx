bdce1a27d7263b2ffa5b5713074493f3310f3070
Roberto Guevara <robertoeguevara@gmail.com>
2023-02-02 17:14:59 -0800

    create formField component

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -15,6 +15,18 @@ const CreatePost = () => {
   const [generatingImg, setGeneratingImg] = useState(false);
   const [loading, setLoading] = useState(false);
 
+  const handleSubmit = () => {
+
+  }
+
+  const handleChange = (e) => {
+
+  }
+
+  const handleSurpriseMe = () => {
+
+  }
+
   return (
     <section className='max-w-7x1 mx-auto'>
       <div>
@@ -26,6 +38,28 @@ const CreatePost = () => {
                 through DALL-E AI and share them with the community
             </p>
         </div>
+        <form className='mt-16 max-w-3x1' onSubmit={handleSubmit}>
+          <div className='flex flex-col gap-5'>
+            <FormField 
+              labelName='Your name'
+              type='text'
+              name='name'
+              placeholder='John Doe'
+              value={form.name}
+              handleChange={handleChange}
+            />
+            <FormField 
+              labelName='Prompt'
+              type='text'
+              name='prompt'
+              placeholder='an armchair in the shape of an avocado'
+              value={form.prompt}
+              handleChange={handleChange}
+              isSurpriseMe
+              handleSurpriseMe={handleSurpriseMe}
+            />
+          </div>
+        </form>
     </section>
   )
 }
