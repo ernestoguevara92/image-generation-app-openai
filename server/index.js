6f791777a562bd5db472bc540a8d494bbe579215
Roberto Guevara <robertoeguevara@gmail.com>
2023-02-04 19:03:33 -0800

    create startServer function

--- server/index.js
+++ server/index.js
@@ -11,3 +11,11 @@ app.use(express.json({ limit: '50mb' }));
 app.get('/', (req, res) => {
   res.send('Hello from DALL-E API');
 });
+
+const startServer = async () => {
+  app.listen(process.env.PORT, () => {
+    console.log(`Server started at http://localhost:${process.env.PORT}`);
+  });
+};
+
+startServer();
