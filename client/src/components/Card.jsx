337ee36360493c5daefd327d33e34444e83a389d
Roberto Guevara <robertoeguevara@gmail.com>
2023-02-09 20:21:44 -0800

    client: start to develop the card component to render images

--- client/src/components/Card.jsx
+++ client/src/components/Card.jsx
@@ -5,7 +5,17 @@ import { downloadImage } from '../utils'
 
 const Card = ({ _id, name, prompt, photo }) => {
   return (
-    <div>Card</div>
+    <div className='rounded-xl group relative shadow-card hover:shadow-cardhover card'>
+      <img 
+        className='w-full h-auto object-cover rounded-xl'
+        src={photo}
+        alt={prompt}
+      />
+      <div className='group-hover:flex flex-col max-h-[94.5%] hidden absolute 
+      bottom-0 left-0 right-0 bg-[#10131f] m-2 p-4 rounded-md'>
+        <p>{prompt}</p>
+      </div>
+    </div>
   )
 }
 
